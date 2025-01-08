fbf1913589c828e5e89432b5537aef1702d386de
Arturo Torres <a7torres@ucsd.edu>
2025-01-08 00:56:07 -0800

    some changes :D

--- cogs/slash_commands.py
+++ cogs/slash_commands.py
@@ -1,37 +1,43 @@
 import disnake
 from disnake.ext import commands
+import sqlite3
 import d20
+from os import getenv
+from dotenv import load_dotenv
 
 class SlashCommands(commands.Cog):
 
     def __init__(self, bot):
-        self.bot = bot
+        self.bot = bot 
 
-    @commands.slash_command(name = 'loot', description = 'Returns loot from monster loot tables for each monster killed')
-    async def loot(self, inter, creature_name, total_creatures):
-
-        rolls = []
-        while len(rolls) != int(total_creatures):
-            roll = d20.roll("1d100")
-            rolls.append(roll.total)
-
-        rarity = []
-        for roll in rolls:
-            if roll == 1:
-                rarity.append("Critical Failure")
-                break
-            elif roll <= 46:
-                rarity.append("Common")
-            elif roll <= 70:
-                rarity.append("Uncommon")
-            elif roll <= 90:
-                rarity.append("Rare")
-            elif roll <= 99:
-                rarity.append("Very Rare")
-            else:
-                rarity.append("Legendary")
-
-        pulls = {rolls[i]: rarity[i] for i in range(len(rolls))}
+    @commands.slash_command(name = 'lootcreature', description = 'Returns loot from monster loot tables for each monster killed')
+    async def lootcreature(self, inter, creature_name, total_creatures):
         
-        await inter.response.send_message(pulls)
+        load_dotenv()
+        
+        def roll_for_n_creatures(total_creatures):
+            rolls = []
+            while len(rolls) != int(total_creatures):
+                roll = d20.roll("1d100")
+                rolls.append(roll.total)
+            return rolls
+
+        creature_name = str(creature_name).capitalize()
+
+        connc = sqlite3.connect(str(getenv('db')))
+        c = connc.cursor()
+        c.execute('SELECT creature_cr FROM creatures WHERE creature_name = :creature_name', {'creature_name': creature_name})
+        creature_cr = c.fetchone()
+        creature_cr = float(creature_cr[0])
+        
+        if creature_cr <= 4:
+            rolls = roll_for_n_creatures(total_creatures)
+        elif creature_cr <= 10:
+            rolls = roll_for_n_creatures(total_creatures)
+        elif creature_cr <=16:
+            rolls = roll_for_n_creatures(total_creatures)
+        else:
+            rolls = roll_for_n_creatures(total_creatures)
+        
+        await inter.response.send_message(rolls)
 
